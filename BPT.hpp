c96027309a44110a58456ba050e5062db9b9df04
YGX-TOUCH-FISH <1816303037@qq.com>
2021-06-04 16:37:34 +0800

    cache

--- BPT.hpp
+++ BPT.hpp
@@ -34,7 +34,7 @@ namespace RA {
     }
 }
 namespace RA {
-    template<typename Key, typename Data, int MAX = 800, int CACHESIZE = 500>
+    template<typename Key, typename Data, int MAX = 200, int CACHESIZE = 50>
     class BPlusTree {
         typedef std::pair<Key, Data> value_type;
 //        static const int MAX = 200;
