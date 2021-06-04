79c6573c0717550a43fdeabd46befadc9d7de931
YGX-TOUCH-FISH <1816303037@qq.com>
2021-06-04 22:58:57 +0800

    faster

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
 //    freopen("data/basic_6/1ans.txt" , "w" , stdout);
 
     std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
     std::string command;
     Init();
     while(getline(cin , command)){
