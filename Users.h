2347414f46055db83ee6fe40c2958330d0c71d80
YGX-TOUCH-FISH <1816303037@qq.com>
2021-06-06 15:44:04 +0800

    15:44

--- Users.h
+++ Users.h
@@ -34,7 +34,7 @@ public:
 class User_Control{
 private:
     BPlusTree<int , User, 200, 5000, 0> username_BPT;//username——User BPT:储存用户信息
-    BPlusTree<int , int, 200, 0, 0> userOrderNum;
+    BPlusTree<int , int, 200, 500, 0> userOrderNum;
 
 public:
     User_Control(){
